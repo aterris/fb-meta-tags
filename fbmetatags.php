ac79cfafffddb768347aebe2b399045abd1fd21e
Andrew Terris <aterris@Andrew-Terriss-iMac.local>
2010-10-14 15:40:11 -0500

    ADD: plugin stub

---
+++ fbmetatags.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: FB Meta Tags
+Plugin URI: http://blueprintds.com
+Description: Ensure your posts are shared correctly on Facebook by setting these meta tags
+Version: 0.1
+Author: Andrew Terris and Eric Marden
+Author URI: http://blueprintds.com
+*/
+
+
+?>
